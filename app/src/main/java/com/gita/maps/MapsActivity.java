eb78102ddbf0de1f4e7705c2fafa3a54dd060546
Gita Nuraini <42348805+GitaNuraini@users.noreply.github.com>
2020-10-31 22:05:14 +0700

    complete

--- app/src/main/java/com/gita/maps/MapsActivity.java
+++ app/src/main/java/com/gita/maps/MapsActivity.java
@@ -1,13 +1,28 @@
 package com.gita.maps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,8 +31,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 //public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-public class MapsActivity extends AppCompatActivity implements LocationListener, View.OnClickListener{
-//    private GoogleMap mMap;
+public class MapsActivity extends AppCompatActivity implements LocationListener, View.OnClickListener, OnMapReadyCallback {
+
     GoogleMap googleMap;
     double latitude;
     double longitude;
@@ -32,11 +47,147 @@ public class MapsActivity extends AppCompatActivity implements LocationListener,
         btn_posisi = (Button) findViewById(R.id.posisi_user);
 
         btn_koordinat.setOnClickListener(this);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
+        btn_posisi.setOnClickListener(this);
+
+        SupportMapFragment fm = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+
+        CekGPS();
+
+        fm.getMapAsync(this);
+        //googleMap.setMyLocationEnabled(true);
+
+        if (latitude != 0 && longitude != 0) {
+
+            Toast.makeText(getApplicationContext(), "Latitude : " + latitude + " Longitude : " + longitude, Toast.LENGTH_LONG).show();
+
+        }
+
     }
+    @Override
+    public void onClick(View v) {
+        if (v == btn_koordinat) {
+            if (latitude != 0 && longitude != 0) {
+                Toast.makeText(getApplicationContext(), "Latitude : " + latitude + " Longitude : " + longitude, Toast.LENGTH_LONG).show();
+            }
+        } else if (v == btn_posisi) {
+            LatLng user = new LatLng(latitude, longitude);
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(user, 12));
+        }
+    }
+
+
+    public void CekGPS() {
+        try {
+            /* Pengecekan GPS hidup / tidak */
+
+            LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Info");
+                builder.setMessage("Anda akan mengaktifkan GPS ?");
+                builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int witch) {
+                        Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(i);
+                    }
+                });
+
+                builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int witch) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.create().show();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
+        // menampilkan status google play service
+
+        if (status != ConnectionResult.SUCCESS) {
+            int requestCode = 10;
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
+            dialog.show();
+        } else {
+            // Google Play Service Tersedia
+            try {
+                LocationManager locationManager = (LocationManager)
+                        getSystemService(LOCATION_SERVICE);
+
+                // Membuat Kriteria Untuk Penumpangan Provider
+                Criteria criteria = new Criteria();
+
+                // Mencari Provider Terbaik
+                String provider = locationManager.getBestProvider(criteria, true);
+
+                // Mendapatkan Lokasi Terakhir
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                Location location = locationManager.getLastKnownLocation(provider);
+
+                if (location != null) {
+                    onLocationChanged(location);
+                }
+                locationManager.requestLocationUpdates(provider, 5000, 0, this);
+
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location lokasi) {
+        //TODO Auto-generated method stub
+        latitude = lokasi.getLatitude();
+        longitude = lokasi.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stud
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //googleMap.setMyLocationEnabled(true);
+        this.googleMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        this.googleMap.setMyLocationEnabled(true);
+    }
+}
 
     /**
      * Manipulates the map once available.
@@ -56,4 +207,3 @@ public class MapsActivity extends AppCompatActivity implements LocationListener,
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 //        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 //    }
-}
